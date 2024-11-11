5744deb5f11565c51d4d2836833ae5a23fbed4a1
Shubham Pandey <shubhampandey3327@gmail.com>
2024-11-12 00:00:08 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     int n;
     printf("Enter number : ");
     scanf("%d", &n);
-    if(n<4){
+    if(n<4 && n>1){
         printf("Prime number\n");
         return 0;
     }
-    for(int i=2;i<n/2;i++){
+    for(int i=2;i<=sqrt(n);i++){
         if(n%i==0){
             printf("not a prime number\n");
             return 0;
