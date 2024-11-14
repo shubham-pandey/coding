273a3be38dd04c1476122f6335db75da260a2aac
Shubham Pandey <shubhampandey3327@gmail.com>
2024-11-14 13:56:29 +0530

    Update frequency.c

--- frequency.c
+++ frequency.c
@@ -21,3 +21,32 @@ int main(){
         printf("%d ",freq[i]);
     }
 }
+/*code by chat got
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+    int digit_count[10] = {0};
+
+    // Read a line of input (up to 999 characters)
+    fgets(str, sizeof(str), stdin);
+
+    // Count each digit in the input
+    for (int i = 0; i < strlen(str); i++) {
+        if (str[i] >= '0' && str[i] <= '9') {
+            digit_count[str[i] - '0']++;
+        }
+    }
+
+    // Print the count of each digit from 0 to 9
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", digit_count[i]);
+    }
+    
+    return 0;
+}
+*/
+
+
+
